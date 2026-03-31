e7a461b3d8e7cb5e20a983025e77f095180610ac
Emily <emilymima.jessica@gmail.com>
2026-03-31 08:40:23 -0300

    fim

--- WidgetEstrutura/sa02_todolist/lib/main.dart
+++ WidgetEstrutura/sa02_todolist/lib/main.dart
@@ -2,10 +2,12 @@
 import 'package:flutter/material.dart';
 
 void main(List<String> args) {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false, //remove a flag do debug
-    home: ToDoList(),
-  ));
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false, //remove a flag do debug
+      home: ToDoList(),
+    ),
+  );
 }
 
 //st -> snipets (atalhos para código)
@@ -24,14 +26,19 @@ class _ToDoListState extends State<ToDoList> {
   //atributos
   //final => permite a mudança de valor uma única vez (escopo da variável)
   // _ o uso do underLine , transforma a variável em private
-  final TextEditingController _tarefaController = TextEditingController();//pega o valor do input
-  final List<Map<String,dynamic>> _tarefas = []; // Lista do Tipo Coleção (Chave, Valor)
+  final TextEditingController _tarefaController =
+      TextEditingController(); //pega o valor do input
+  final List<Map<String, dynamic>> _tarefas =
+      []; // Lista do Tipo Coleção (Chave, Valor)
 
   //métodos
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Lista de Tarefas"),centerTitle: true,),//centraliza o texto no meio da Appbar
+      appBar: AppBar(
+        title: Text("Lista de Tarefas"),
+        centerTitle: true,
+      ), //centraliza o texto no meio da Appbar
       body: Padding(
         //espaçamento geral de 8px
         padding: EdgeInsets.all(8),
@@ -39,54 +46,73 @@ class _ToDoListState extends State<ToDoList> {
           children: [
             //input para adicionar novas tarefas
             TextField(
-              controller: _tarefaController,// passa o valor do texto para o controller
-              decoration: InputDecoration(
-                labelText: "Digite uma Tarefa"
-              ),
+              controller:
+                  _tarefaController, // passa o valor do texto para o controller
+              decoration: InputDecoration(labelText: "Digite uma Tarefa"),
             ),
-            SizedBox(height: 10,), //espaçamento de altura
-            ElevatedButton( // botão para adicioanr tarefa
-              onPressed: _adicionarTarefa, 
-              child: Text("Adicionar Tarefa")),
+            SizedBox(height: 10), //espaçamento de altura
+            ElevatedButton(
+              // botão para adicioanr tarefa
+              onPressed: _adicionarTarefa,
+              child: Text("Adicionar Tarefa"),
+            ),
             //campo para listar as tarefas
             Expanded(
               //Listar as Tarefas da Coleção
               child: ListView.builder(
                 itemCount: _tarefas.length, //conta o nº de item na lista
-                itemBuilder: (context,index)=>
-                  //exibe o elemento da Lista
-                  ListTile(
-                    title: Text(_tarefas[index]["titulo"], style: TextStyle(
-                      //operador Ternário (if,else) => se tarefa concluida , coloca um risco no texto
-                      decoration: _tarefas[index]["concluida"] ? TextDecoration.lineThrough : null
-                    ),),
-                    leading: Checkbox( //permite mudar o valor da tarefa para concluida ou o contrário
-                      value: _tarefas[index]["concluida"], 
-                      onChanged: (bool? valor)=> setState(() {
-                        _tarefas[index]["concluida"] = valor!;
-                      })),
-                      //botão para excluir a tarefa da lista
-                    trailing: IconButton(
-                      icon: Icon(Icons.delete),
-                      onPressed: () => setState(() {
-                        _tarefas.removeAt(index);
-                      }),
+                itemBuilder: (context, index) =>
+                    //exibe o elemento da Lista
+                    ListTile(
+                      title: Text(
+                        _tarefas[index]["titulo"],
+                        style: TextStyle(
+                          //operador Ternário (if,else) => se tarefa concluida , coloca um risco no texto
+                          decoration: _tarefas[index]["concluida"]
+                              ? TextDecoration.lineThrough
+                              : null,
+                        ),
+                      ),
+                      leading: Checkbox(
+                        //permite mudar o valor da tarefa para concluida ou o contrário
+                        value: _tarefas[index]["concluida"],
+                        onChanged: (bool? valor) => setState(() {
+                          //chamando a mudança de estado
+                          _tarefas[index]["concluida"] = valor!;
+                        }),
+                      ),
+                      // coloquem um icone( de lixeira), ao ser clicado vai deletar a tarefa
+                      //usar o trailing para colocar o icone da lixeira
+                      trailing: ElevatedButton(
+                        onPressed: () => _deletarTarefa,
+                        child: Icon(Icons.delete),
+                      ),
                     ),
-                  ),
               ),
-            )
+            ),
           ],
-        ),),
+        ),
+      ),
     );
   }
+
   //método para adicionar tarefa na lista
   void _adicionarTarefa() {
-    if(_tarefaController.text.trim().isNotEmpty){
+    if (_tarefaController.text.trim().isNotEmpty) {
       setState(() {
+        // chama a mudança de estado da janela
         //adiciona a tarefa na lista
-        _tarefas.add({"titulo":_tarefaController.text,"concluida":false});
+        _tarefas.add({"titulo": _tarefaController.text, "concluida": false});
         //limpa o campo do input
         _tarefaController.clear();
+      });
+    }
+  }
+
+  void _deletarTarefa(int index) {
+    if(_tarefas[index]["concuilda"]){
+      setState(() {
+        _tarefas.removeAt(index);
       });
     }
   }
