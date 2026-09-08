5b21c903b8b3fff0cfe419f8f87c95547f443699
spidemyer <emilymima.jessica@gmail.com>
2026-09-08 11:41:37 -0300

    wifi

---
+++ RecursosHardware/exemplo_wifi_connectivity/lib/main.dart
@@ -0,0 +1,22 @@
+// biblioteca wifi_connetivity_plus
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MaterialApp(home: MyApp(),));
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
